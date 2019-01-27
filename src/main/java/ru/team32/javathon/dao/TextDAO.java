934559dd7f9b2f1a16c5f9d180545b33a1ed394e
syber <ninefoldcomplex@gmail.com>
2019-01-27 06:19:51 +0300

    theme controller

---
+++ src/main/java/ru/team32/javathon/dao/TextDAO.java
@@ -0,0 +1,20 @@
+package ru.team32.javathon.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate4.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+import ru.team32.javathon.entity.Theme;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+public class TextDAO implements ITextDAO {
+    @Autowired
+    private HibernateTemplate hibernateTemplate;
+
+    @Override
+    public void getSomething() {
+        System.out.println(hibernateTemplate.get(Theme.class, 1L));
+    }
+}
